dbcd1b0b0c08fbe7f59f350f85c693876f79c128
piotr.idzik <vil02@o2.pl>
2021-07-16 15:56:44 +0200

    improved error handling

--- ubuntu_local_par_build.sh
+++ ubuntu_local_par_build.sh
@@ -10,9 +10,9 @@ cd latex/ || exit 126
 
 readonly FILE_NAME="pi2021"
 readonly FILE_EXT=".tex"
-pdflatex -interaction=batchmode -draftmode "$FILE_NAME$FILE_EXT"
-bibtex $FILE_NAME
-pdflatex -interaction=batchmode -draftmode "$FILE_NAME$FILE_EXT"
+pdflatex -interaction=batchmode -draftmode "$FILE_NAME$FILE_EXT" || true
+bibtex $FILE_NAME || true
+pdflatex -interaction=batchmode -draftmode "$FILE_NAME$FILE_EXT" || true
 pdflatex -interaction=batchmode "$FILE_NAME$FILE_EXT"
 
 cd ..
